f09dd907bf55883ea1c20ba9364643a6140b10f8
Dan Cahoon <dpcahoon@gmail.com>
2021-04-07 11:30:32 -0400

    Add GLFHC sites

--- lib/sites/acuity.rb
+++ lib/sites/acuity.rb
@@ -66,6 +66,66 @@ module Acuity
       },
     },
 
+    '700 Essex St. (GLFHC patients only) in Lawrence, MA' => {
+      sign_up_url: 'https://glfhc.org/covid-19-vaccine-signup-form/',
+      api_url: 'https://glfhccovid19iz.as.me/schedule.php?action=showCalendar&fulldate=1&owner=21956779&template=weekly&location=700+Essex+Street%2C+Lawrence+MA',
+      api_params: {
+        type: 20223228,
+        calendar: 5075244,
+        skip: true,
+        'options[qty]' => 1,
+        'options[numDays]' => 27,
+        ignoreAppointment: '',
+        appointmentType: '',
+        calendarID: '',
+      },
+    },
+
+    '147 Pelham St. in Methuen, MA' => {
+      sign_up_url: 'https://glfhc.org/covid-19-vaccine-signup-form/',
+      api_url: 'https://glfhccovid19iz.as.me/schedule.php?action=showCalendar&fulldate=1&owner=21956779&template=weekly',
+      api_params: {
+        type: 20223228,
+        calendar: 5114836,
+        skip: true,
+        'options[qty]' => 1,
+        'options[numDays]' => 27,
+        ignoreAppointment: '',
+        appointmentType: '',
+        calendarID: 5114836,
+      },
+    },
+
+    'Central Plaza - 2 Water St. in Haverhill, MA' => {
+      sign_up_url: 'https://glfhc.org/covid-19-vaccine-signup-form/',
+      api_url: 'https://glfhccovid19iz.as.me/schedule.php?action=showCalendar&fulldate=1&owner=21956779&template=weekly&location=Central+Plaza%2C+2+Water+Street%2C+Haverhill+MA',
+      api_params: {
+        type: 20223228,
+        calendar: 5236989,
+        skip: true,
+        'options[qty]' => 1,
+        'options[numDays]' => 27,
+        ignoreAppointment: '',
+        appointmentType: '',
+        calendarID: '',
+      },
+    },
+
+    'Northern Essex Community College 45 Franklin St. (Lawrence residents only) in Lawrence, MA' => {
+      sign_up_url: 'https://glfhc.org/covid-19-vaccine-signup-form/',
+      api_url: 'https://glfhccovid19iz.as.me/schedule.php?action=showCalendar&fulldate=1&owner=21956779&template=weekly&location=45+Franklin+Street%2C+Lawrence+MA',
+      api_params: {
+        type: 20223228,
+        calendar: 5341082,
+        skip: true,
+        'options[qty]' => 1,
+        'options[numDays]' => 27,
+        ignoreAppointment: '',
+        appointmentType: '',
+        calendarID: '',
+      },
+    },
+
     # NOTE: Trinity is showing a message that all appointment types are private
     #'Trinity EMS in Haverhill, MA' => {
       #sign_up_url: 'https://trinityems.com/what-we-do/covid-19-vaccine-clinics/',

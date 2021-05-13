a8a700b3bc719da7be10d2703407af2e9c5c9909
Dan Cahoon <dpcahoon@gmail.com>
2021-05-12 22:58:58 -0400

    Fix tests

--- spec/ma_immunizations_spec.rb
+++ spec/ma_immunizations_spec.rb
@@ -16,10 +16,10 @@ describe 'MaImmunizations' do
       clinics = MaImmunizations.all_clinics(storage, logger)
       # NOTE(dan): there are 8 clinics in the example file but one is a
       # duplicate that we consolidate, so we only expect to have 7
-      expect(clinics.length).to eq(7)
+      expect(clinics.length).to eq(3)
       first_clinic = clinics[0]
-      expect(first_clinic.appointments).to eq(100)
-      expect(first_clinic.title).to eq('Reggie Lewis State Track Athletic Ctr, Tremont Street, Boston, MA, USA on 03/01/2021')
+      expect(first_clinic.appointments).to eq(124)
+      expect(first_clinic.title).to eq('Citizen Center on 05/13/2021')
     end
 
     it 'can work with twitter' do
@@ -30,9 +30,11 @@ describe 'MaImmunizations' do
       expect(RestClient).to receive(:get).and_return(response)
       expect(redis).to receive(:get).with('vaccine-cookies:ma-immunization').and_return({ cookies: 'foo', expiration: Time.now + (60 * 60 * 24) }.to_json)
       clinics = MaImmunizations.all_clinics(storage, logger)
-      expect(redis).to receive(:get).exactly(3).times.and_return(nil)
-      expect(redis).to receive(:set).once
-      expect(mock_twitter).to receive(:update).with('100 appointments available at Reggie Lewis State Track Athletic Ctr, Tremont Street, Boston, MA, USA in Boston, MA on 03/01/2021 for Pfizer-BioNTech COVID-19 Vaccine. Check eligibility and sign up at https://www.maimmunizations.org/clinic/search?q[venue_search_name_or_venue_name_i_cont]=Reggie%20Lewis%20State%20Track%20Athletic%20Ctr,%20Tremont%20Street,%20Boston,%20MA,%20USA&')
+      allow(redis).to receive(:get).and_return(nil)
+      allow(redis).to receive(:set)
+      expect(mock_twitter).to receive(:update).with('124 appointments available at Citizen Center in Haverhill, MA on 05/13/2021 for Moderna COVID-19 Vaccine, Janssen COVID-19 Vaccine. Check eligibility and sign up at https://www.maimmunizations.org/appointment/en/clinic/search?q[venue_search_name_or_venue_name_i_cont]=Citizen%20Center&')
+      expect(mock_twitter).to receive(:update).with('23 appointments available at Citizen Center in Haverhill, MA on 05/20/2021 for Moderna COVID-19 Vaccine. Check eligibility and sign up at https://www.maimmunizations.org/appointment/en/clinic/search?q[venue_search_name_or_venue_name_i_cont]=Citizen%20Center&')
+      expect(mock_twitter).to receive(:update).with('79 appointments available at Citizen Center in Haverhill, MA on 05/27/2021 for Moderna COVID-19 Vaccine. Check eligibility and sign up at https://www.maimmunizations.org/appointment/en/clinic/search?q[venue_search_name_or_venue_name_i_cont]=Citizen%20Center&')
       twitter.post(clinics)
     end
   end
